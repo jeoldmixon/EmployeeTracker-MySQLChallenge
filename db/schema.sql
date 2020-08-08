709a8e891f774c4758c2f7197c179f07c616865d
jeoldmixon <jeoldmixon@yahoo.com>
2020-08-08 15:22:16 -0500

    added seed info

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,36 @@
-CREATE TABLE candidates (
-  id INTEGER PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+-- Department
+-- id: INT PRIMARY KEY
+-- name: VARCHAR(30) to hold department name
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+-- Role
+-- id: INT PRIMARY KEY
+-- title: VARCHAR(30) to hold role title
+-- salary: DECIMAL to hold role salary
+-- department_id: INT to hold reference to department role belongs to
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
+);
+
+-- Employee
+-- id: INT PRIMARY KEY
+-- first_name: VARCHAR(30) to hold employee first name
+-- last_name: VARCHAR(30) to hold employee last name
+-- role_id: INT to hold reference to employee role
+-- manager_id: INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,53 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
+-- You may want to use a separate file containing functions for performing specific SQL queries you'll need to use. A constructor function or class could be helpful for organizing these. You may also want to include a seeds.sql file to pre-populate your database. This will make the development of individual features much easier.
+
+
+
+-- _________________________________________________________________________________
+-- CREATE TABLE department (id INT PRIMARY KEY, name VARCHAR(30));
+
+INSERT INTO department (name) VALUES
+('Executive'),
+('Office'),
+('IT'),
+('Field Staff');
+-- _________________________________________________________________________________
+-- CREATE TABLE role (id INT PRIMARY KEY, title VARCHAR(30), salary DECIMAL, department_id INT);
+
+INSERT INTO role (title,salary,department_id) VALUES
+('President & CEO', 150000.00,1),
+('Vice-President & COO',100000,1),
+('Accountant',70000,2),
+('Accounting Asst',40000,2),
+('HR Rep',65000,2),
+('HR Asst',40000,2)
+('IT Lead', 75000,3),
+('IT Support',55000,3),
+('Field Paramedic Manager',55000,4),
+('Paramedic',45000,4),
+('EMT',29000,4);
+
+-- 9 is PFS
+-- _________________________________________________________________________________
+-- CREATE TABLE employee (id INT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT,
+-- manager_id INT,);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 0),
-  ('Charles', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+  ('Ronald', 'Firbank', 1,1),
+  ('Virginia', 'Woolf', 2,1),
+  ('Piers', 'Gaveston', 3,2),
+  ('Charles', 'LeRoi', 4,3),
+  ('Katherine', 'Mansfield', 5,2),
+  ('Dora', 'Carrington', 6,5),
+  ('Edward', 'Bellamy', 7,2),
+  ('Montague', 'Summers', 8,7),
+  ('Octavia', 'Butler', 9,2),
+  ('Unica', 'Zurn', 10,9),
+  ('Justin','Smith',10,9),
+  ('Jane','Smith',11,9),
+  ('Charles','Sumner',11,9),
+  ('Janie', 'Zurn', 10,9),
+  ('Juan','Zuniga',10,9),
+  ('Jose','Guerrero',11,9),
+  ('Carlos','Zapata',11,9);
